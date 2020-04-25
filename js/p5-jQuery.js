01a7a907dfe7fd765d3c9ff32b40b51a9c5f43ee
jordanHull <jhull4@gmu.edu>
2020-04-24 22:32:06 -0400

    p5-I MAKE FRUIT

---
+++ js/p5-jQuery.js
@@ -0,0 +1,16 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+var images = ["p5-pictures/fruit-1.png","p5-pictures/fruit-2.png","p5-pictures/fruit-3.png","p5-pictures/fruit-4.png","p5-pictures/fruit-5.png","p5-pictures/fruit-6.png","p5-pictures/fruit-7.png","p5-pictures/fruit-8.png","p5-pictures/fruit-9.png","p5-pictures/fruit-10.png","p5-pictures/fruit-11.png","p5-pictures/fruit-12.png","p5-pictures/fruit-13.png","p5-pictures/fruit-14.png","p5-pictures/fruit-15.png","p5-pictures/fruit-16.png","p5-pictures/fruit-17.png","p5-pictures/fruit-18.png","p5-pictures/fruit-19.png","p5-pictures/fruit-20.png","p5-pictures/fruit-21.png","p5-pictures/fruit-22.png","p5-pictures/fruit-23.png","p5-pictures/fruit-24.png","p5-pictures/fruit-25.png","p5-pictures/fruit-26.png"];
+
+var random = getRandomInt(1, 24);
+var randomVal = images[random];
+function makeFruit() {
+  var fruitPic= document.createElement("IMG");
+  fruitPic.setAttribute("src", randomVal);
+  fruitPic.setAttribute("width", "400");
+  fruitPic.setAttribute("height", "400");
+  document.body.appendChild(fruitPic);
+}
